856d0ba2a052c34867cd69b5d549d56ed51699f7
renatosalvatore <renatosalvatore@gmail.com>
2023-10-28 10:03:13 -0300

    Api Music
    1a carga al repositorio git

---
+++ src/Global/Global.js
@@ -0,0 +1,12 @@
+var Global = {
+	playlistLimit: 50,
+	songLimit: 100,
+	access_token: "",
+	refresh_token: "",
+	redirect_uri : "http://localhost:3000/",
+	client_id : "180730f3f5a04a6dad16fc6e5c92fe86",
+	client_secret : "bde4adae43444f8da391e15e52e7cbd8",
+	scopes : "user-read-private user-read-email playlist-read-private"
+}
+
+export default Global;
