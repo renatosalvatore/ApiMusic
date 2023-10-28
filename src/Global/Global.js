14ee698ea251216b46b138f4a789846c8ef5d610
renatosalvatore <renatosalvatore@gmail.com>
2023-10-28 10:47:32 -0300

    cambio uri

--- src/Global/Global.js
+++ src/Global/Global.js
@@ -3,7 +3,7 @@ var Global = {
 	songLimit: 100,
 	access_token: "",
 	refresh_token: "",
-	redirect_uri : "http://localhost:3000/",
+	redirect_uri : "https://apimusic.netlify.app/",
 	client_id : "180730f3f5a04a6dad16fc6e5c92fe86",
 	client_secret : "bde4adae43444f8da391e15e52e7cbd8",
 	scopes : "user-read-private user-read-email playlist-read-private"
